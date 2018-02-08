dbdfc5b6a92ac7126d3ef3194800f7a772967dd3
GFG1\fengjac <fengjac@esquel.com>
2018-02-08 15:06:40 +0800

    no problem 3

--- src/container.js
+++ src/container.js
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter, Switch, Route, Redirect } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import ImmutablePropTypes from 'react-immutable-proptypes'
 
 import PageHeader from './global/pageHeader/components/PageHeader.jsx'
 import Development from './development/components/Development.jsx'
@@ -14,6 +15,7 @@ import * as developmentActions from './development/actions'
 import * as shoppingCartActions from './shoppingCart/actions'
 import * as joAssignActions from './joAssign/actions'
 import * as pageHeaderActions from './global/pageHeader/actions'
+import * as globalActions from './global/actions'
 
 class MainContainer extends PureComponent {
     constructor(props) {
@@ -25,6 +27,12 @@ class MainContainer extends PureComponent {
     }
 
     componentWillMount() {
+        this.props.actions.global.searchFinishing()
+        this.props.actions.global.getShipMode()
+        this.props.actions.global.getDestination()
+        this.props.actions.global.getWashType()
+        this.props.actions.global.getGarmentFeature()
+        this.props.actions.global.getUserProfile()
         console.log('MainContainer componentWillMount')
     }
 
@@ -42,13 +50,7 @@ class MainContainer extends PureComponent {
 
     render() {
         const { state, actions } = this.props
-        const { development, shoppingCart, joAssign, pageHeader } = state
-        console.debug('main container state: ', {
-            development: development.toJS(),
-            shoppingCart: shoppingCart.toJS(),
-            joAssign: joAssign.toJS(),
-            pageHeader: pageHeader.toJS(),
-        })
+        const { development, shoppingCart, joAssign, pageHeader, global } = state
 
         return (
             <div>
@@ -66,8 +68,14 @@ class MainContainer extends PureComponent {
                         actions={{ ...actions.shoppingCart }}
                     />} />
                     <Route path="/joAssign" render={() => <JoAssign
-                        state={{ joAssign }}
-                        actions={{ ...actions.joAssign }}
+                        state={{
+                            joAssign,
+                            masterDataState: global.get('masterDataState'),
+                        }}
+                        actions={{
+                            ...actions.joAssign,
+                            updateBreadcrumb: actions.pageHeader.updateBreadcrumb,
+                        }}
                     />} />
                     <Redirect to="/development/fabricitem" />
                 </Switch>
@@ -77,7 +85,171 @@ class MainContainer extends PureComponent {
 }
 
 MainContainer.propTypes = {
-
+    // state: PropTypes.shape({
+    // shoppingCart: ImmutablePropTypes.contains({
+    //     handLoomState: ImmutablePropTypes.contains({
+    //         leftContainerState: ImmutablePropTypes.contains({
+    //             isExpanded: PropTypes.bool.isRequired,
+    //         }).isRequired,
+    //     }).isRequired,
+    //     trialWeaveState: ImmutablePropTypes.contains({
+    //         leftContainerState: ImmutablePropTypes.contains({
+    //             isExpanded: PropTypes.bool.isRequired,
+    //         }).isRequired,
+    //     }).isRequired,
+    //     fcrState: ImmutablePropTypes.contains({
+    //         leftContainerState: ImmutablePropTypes.contains({
+    //             isExpanded: PropTypes.bool.isRequired,
+    //         }).isRequired,
+    //     }).isRequired,
+    //     sheetSelectorState: ImmutablePropTypes.contains({
+    //         sheets: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //             text: PropTypes.string.isRequired,
+    //             value: PropTypes.string.isRequired,
+    //         })),
+    //         selectedSheet: PropTypes.string.isRequired,
+    //     }).isRequired,
+    // }),
+    // joAssign: ImmutablePropTypes.contains({
+    //     leftContainerState: ImmutablePropTypes.contains({
+    //         requestList: ImmutablePropTypes.listOf(ImmutablePropTypes.map.isRequired).isRequired,
+    //         isExpanded: PropTypes.bool.isRequired,
+    //         isFetchingRequestList: PropTypes.bool.isRequired,
+    //     }).isRequired,
+    //     rightContainerState: ImmutablePropTypes.contains({
+    //         fileContent: ImmutablePropTypes.map.isRequired,
+    //         isFetchingRequestFile: PropTypes.bool.isRequired,
+    //     }).isRequired,
+    //     sheetSelectorState: ImmutablePropTypes.contains({
+    //         sheets: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //             text: PropTypes.string.isRequired,
+    //             value: PropTypes.string.isRequired,
+    //         })),
+    //         selectedSheet: PropTypes.string.isRequired,
+    //     }).isRequired,
+    // }),
+    // global: ImmutablePropTypes.contains({
+    //     apiRequestState: ImmutablePropTypes.contains({
+    //         apiRequestsForBlockUi: PropTypes.number.isRequired,
+    //     }).isRequired,
+    //     masterDataState: ImmutablePropTypes.contains({
+    //         finishingList: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //             value: PropTypes.string.isRequired,
+    //             label: PropTypes.string.isRequired,
+    //         })).isRequired,
+    //         washTypeList: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //             value: PropTypes.string.isRequired,
+    //             label: PropTypes.string.isRequired,
+    //         })).isRequired,
+    //         garmentFeatureList: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //             value: PropTypes.string.isRequired,
+    //             label: PropTypes.string.isRequired,
+    //         })).isRequired,
+    //         destinationList: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //             value: PropTypes.string.isRequired,
+    //             label: PropTypes.string.isRequired,
+    //         })).isRequired,
+    //         shipModeList: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //             value: PropTypes.string.isRequired,
+    //             label: PropTypes.string.isRequired,
+    //         })).isRequired,
+    //     }).isRequired,
+    //     userErrorState: ImmutablePropTypes.contains({
+    //         userErrors: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //             title: PropTypes.string,
+    //             message: PropTypes.any,
+    //             alertType: PropTypes.string,
+    //             index: PropTypes.number.isRequired,
+    //             html: PropTypes.string,
+    //         })).isRequired,
+    //     }).isRequired,
+    //     userProfileState: ImmutablePropTypes.contains({
+    //         customerList: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //             customerName: PropTypes.string.isRequired,
+    //             customerCode: PropTypes.string.isRequired,
+    //         })).isRequired,
+    //         brandList: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //             brandName: PropTypes.string.isRequired,
+    //             brandCode: PropTypes.string.isRequired,
+    //         })).isRequired,
+    //         selectedCustomer: PropTypes.string.isRequired,
+    //         selectedBrand: PropTypes.string.isRequired,
+    //         userProfile: ImmutablePropTypes.contains({
+    //             _id: PropTypes.string,
+    //             userName: PropTypes.string,
+    //             customers: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //                 customerCode: PropTypes.string.isRequired,
+    //                 customerName: PropTypes.string.isRequired,
+    //                 brands: ImmutablePropTypes.listOf(ImmutablePropTypes.contains({
+    //                     brandCode: PropTypes.string.isRequired,
+    //                     brandName: PropTypes.string.isRequired,
+    //                 })).isRequired,
+    //             })),
+    //         }).isRequired,
+    //     }).isRequired,
+    // }).isRequired,
+    // pageHeader: ImmutablePropTypes.contains({
+    //     menu: PropTypes.string.isRequired,
+    //     menuItemSet: ImmutablePropTypes.listOf(PropTypes.string.isRequired).isRequired,
+    //     systemMenu: ImmutablePropTypes.listOf(ImmutablePropTypes.map.isRequired).isRequired,
+    //     breadcrumb: ImmutablePropTypes.listOf(PropTypes.string.isRequired).isRequired,
+    //     sites: ImmutablePropTypes.listOf(PropTypes.string.isRequired).isRequired,
+    //     menuItems: ImmutablePropTypes.listOf(PropTypes.string.isRequired).isRequired,
+    // }),
+    // }),
+    // actions: PropTypes.shape({
+    // shoppingCart: PropTypes.shape({
+    //     shoppingCartActions: PropTypes.shape({
+    //         onChangeSheet: PropTypes.func.isRequired,
+    //         onChangeShoppingCartHeader: PropTypes.func.isRequired,
+    //         onFocusShoppingCartTextarea: PropTypes.func.isRequired,
+    //         onBlurShoppingCartTextarea: PropTypes.func.isRequired,
+    //     }).isRequired,
+    //     handLoomActions: PropTypes.shape({
+    //         onExpandDirectory: PropTypes.func.isRequired,
+    //         onCollapseDirectory: PropTypes.func.isRequired,
+    //     }).isRequired,
+    //     trialWeaveActions: PropTypes.shape({
+    //         onExpandDirectory: PropTypes.func.isRequired,
+    //         onCollapseDirectory: PropTypes.func.isRequired,
+    //     }).isRequired,
+    //     fcrActions: PropTypes.shape({
+    //         onExpandDirectory: PropTypes.func.isRequired,
+    //         onCollapseDirectory: PropTypes.func.isRequired,
+    //     }).isRequired,
+    // }),
+    // joAssign: PropTypes.shape({
+    //     onClickFolder: PropTypes.func.isRequired,
+    //     onExpandDirectory: PropTypes.func.isRequired,
+    //     onCollapseDirectory: PropTypes.func.isRequired,
+    //     onFetchHandloomRequestList: PropTypes.func.isRequired,
+    //     onOpenHandloomRequest: PropTypes.func.isRequired,
+    //     onClickSave: PropTypes.func.isRequired,
+    //     onClickProceed: PropTypes.func.isRequired,
+    //     onChangeSheet: PropTypes.func.isRequired,
+    // }),
+    // pageHeader: PropTypes.shape({
+    //     getSystemMenu: PropTypes.func.isRequired,
+    //     selectMenuItem: PropTypes.func.isRequired,
+    //     addMenuItemSet: PropTypes.func.isRequired,
+    //     cleanMenuItemSet: PropTypes.func.isRequired,
+    //     updateBreadcrumb: PropTypes.func.isRequired,
+    // }),
+    // global: PropTypes.shape({
+    //     onAddUserErrorForAlert: PropTypes.func.isRequired,
+    //     onRemoveUserErrorForAlert: PropTypes.func.isRequired,
+    //     searchFinishing: PropTypes.func.isRequired,
+    //     getShipMode: PropTypes.func.isRequired,
+    //     getDestination: PropTypes.func.isRequired,
+    //     getWashType: PropTypes.func.isRequired,
+    //     getGarmentFeature: PropTypes.func.isRequired,
+    //     onAddAPIRequestForBlockUi: PropTypes.func.isRequired,
+    //     onRemoveAPIRequestForBlockUi: PropTypes.func.isRequired,
+    //     getUserProfile: PropTypes.func.isRequired,
+    //     onChangeSelectedCustomer: PropTypes.func.isRequired,
+    //     onChangeSelectedBrand: PropTypes.func.isRequired,
+    // }),
+    // }),
 }
 
 const mapStateToProps = state => {
@@ -101,6 +273,7 @@ const mapDispatchToProps = dispatch => {
             shoppingCart: bindActionCreators({ ...shoppingCartActions, }, dispatch),
             joAssign: bindActionCreators({ ...joAssignActions, }, dispatch),
             pageHeader: bindActionCreators({ ...pageHeaderActions, }, dispatch),
+            global: bindActionCreators({ ...globalActions, }, dispatch),
         }
     }
 }
